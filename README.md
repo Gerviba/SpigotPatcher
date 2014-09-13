5721901c509288ae253a2d4d28ea9ad9c928beec
Gerviba <gerviba@gerviba.hu>
2014-09-13 17:40:50 +0200

    Added a better GUI

--- README.md
+++ README.md
@@ -1,26 +1,3 @@
-SpigotPatcher
-=============
-
-**Binary patcher for distribution of Spigot and related updates.**
-
-Usage
------
-See the [initial release announcement](http://www.spigotmc.org/threads/29091/) or [this wiki page](http://www.spigotmc.org/wiki/spigot-patcher/) for detailed usage instructions on how to use SpigotPatcher. For command line invocation, simply run the program without any arguments.
-
-Changelog
----------
-
-* **1.0.1**: Check file read and write permissions before beginning patch process. 
-* **1.0.0**: Initial Release
-
-Download
---------
-You can grab downloads of current and past versions from our [Jenkins server](http://ci.md-5.net/job/SpigotPatcher/).
-
-TODO
-----
-* Simple and easy to use GUI.
-
 SpigotPatcherGUI
 =============
 
@@ -37,6 +14,6 @@ GUI for the SpigotPatcher app!
  * If you start the application with **--nogui** argument, you can use the original console based patcher.
  
 ##JVM arguments:##
- * If you start the application with **-Dlanguage=<lang>** JVM argument, your patcher will start with the selected language.
+ * If you start the application with **-Dlanguage=en** JVM argument, your patcher will start with the selected language.
  
 ![ScreenShot](http://content.screencast.com/users/Gerviba/folders/Jing/media/e1c68032-b4fb-49ab-bea7-d6289f738a8e/2014-09-13_1550.png)
--- src/main/java/org/spigotmc/patcher/Main.java
+++ src/main/java/org/spigotmc/patcher/Main.java
@@ -2,91 +2,72 @@ package org.spigotmc.patcher;
 
 import com.google.common.hash.Hashing;
 import com.google.common.io.Files;
-import java.awt.GraphicsEnvironment;
 import java.io.File;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
 import net.md_5.jbeat.Patcher;
 
 public class Main
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        if ( !GraphicsEnvironment.isHeadless() && args.length == 0 )
-        {
-            UserInterface.main( args );
-            return;
-        }
-
-        if ( args.length != 3 )
+    	if((args.length > 0 && !args[0].equalsIgnoreCase("--nogui")) || args.length == 0) {
+    		System.out.println( "Init GUI" );
+    		GuiPatcher.init();
+    		return;
+    	}
+    	
+        if ( args.length != 4 )
         {
             System.out.println( "Welcome to the Spigot patch applicator." );
-            System.out.println( "In order to use this tool you will need to specify three command line arguments as follows:" );
-            System.out.println( "\tjava -jar SpigotPatcher.jar original.jar patch.bps output.jar" );
+            System.out.println( "In order to use this tool you will need to specify four command line arguments as follows:" );
+            System.out.println( "\tjava -jar SpigotPatcher.jar --nogui original.jar patch.bps output.jar" );
             System.out.println( "This will apply the specified patch to the original jar and save it to the output jar" );
             System.out.println( "Please ensure that you save your original jar for later use." );
             System.out.println( "If you have any queries, please direct them to http://www.spigotmc.org/" );
             return;
         }
 
-        patchSafe( new PrintWriter( System.out ), new File( args[0] ), new File( args[1] ), new File( args[2] ) );
-    }
+        File originalFile = new File( args[1] );
+        File patchFile = new File( args[2] );
+        File outputFile = new File( args[3] );
 
-    @SuppressWarnings("TooBroadCatch")
-    public static void patchSafe(PrintWriter console, File originalFile, File patchFile, File outputFile)
-    {
-        try
-        {
-            patch( console, originalFile, patchFile, outputFile );
-        } catch ( Exception ex )
-        {
-            console.println( "***** Unknown error occured during patch process:" );
-            ex.printStackTrace( console );
-        }
-    }
-
-    @SuppressWarnings("TooBroadCatch")
-    public static void patch(PrintWriter console, File originalFile, File patchFile, File outputFile) throws IOException
-    {
         if ( !originalFile.canRead() )
         {
-            console.println( "Specified original file " + originalFile + " does not exist or cannot be read!" );
+            System.err.println( "Specified original file " + originalFile + " does not exist or cannot be read!" );
             return;
         }
         if ( !patchFile.canRead() )
         {
-            console.println( "Specified patch file " + patchFile + " does not exist or cannot be read!!" );
+            System.err.println( "Specified patch file " + patchFile + " does not exist or cannot be read!!" );
             return;
         }
         if ( outputFile.exists() )
         {
-            console.println( "Specified output file " + outputFile + " exists, please remove it before running this program!" );
+            System.err.println( "Specified output file " + outputFile + " exists, please remove it before running this program!" );
             return;
         }
         if ( !outputFile.createNewFile() )
         {
-            console.println( "Could not create specified output file " + outputFile + " please ensure that it is in a valid directory which can be written to." );
+            System.out.println( "Could not create specified output file " + outputFile + " please ensure that it is in a valid directory which can be written to." );
             return;
         }
 
-        console.println( "***** Starting patching process, please wait." );
-        console.println( "\tInput md5 Checksum: " + Files.hash( originalFile, Hashing.md5() ) );
-        console.println( "\tPatch md5 Checksum: " + Files.hash( patchFile, Hashing.md5() ) );
+        System.out.println( "***** Starting patching process, please wait." );
+        System.out.println( "\tInput md5 Checksum: " + Files.hash( originalFile, Hashing.md5() ) );
+        System.out.println( "\tPatch md5 Checksum: " + Files.hash( patchFile, Hashing.md5() ) );
 
         try
         {
             new Patcher( patchFile, originalFile, outputFile ).patch();
         } catch ( Exception ex )
         {
-            console.println( "***** Exception occured whilst patching file!" );
-            ex.printStackTrace( console );
+            System.err.println( "***** Exception occured whilst patching file!" );
+            ex.printStackTrace();
             outputFile.delete();
             return;
         }
 
-        console.println( "***** Your file has been patched and verified! We hope you enjoy using Spigot!" );
-        console.println( "\tOutput md5 Checksum: " + Files.hash( outputFile, Hashing.md5() ) );
+        System.out.println( "***** Your file has been patched and verified! We hope you enjoy using Spigot!" );
+        System.out.println( "\tOutput md5 Checksum: " + Files.hash( outputFile, Hashing.md5() ) );
     }
 }
